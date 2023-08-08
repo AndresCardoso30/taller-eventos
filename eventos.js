c9379c3dd20e1484ab04b8e0a982dbbee5d437d6
Andres Cardoso <andress.cardosoo@gmail.com>
2023-08-08 16:58:24 -0300

    Agregamos div

---
+++ eventos.js
@@ -0,0 +1,23 @@
+ // let fecha = new Date(); //
+
+ const btn = document.getElementById('apretar');
+
+ const div = document.getElementById('div1')
+ /* 
+ function saludar() {
+     const fecha = new Date();
+     if (fecha.getHours() >= 8 && fecha.getHours() < 20) {
+         alert("Buenos días, bienvenido/a a mi página");
+     } else {
+         alert("Buenas noches, bienvenido/a a mi página");
+     }
+ }
+ */
+ 
+ function saludar() {
+     alert("Hola!")
+ }
+ 
+ div.addEventListener("click", function () {
+     alert("Hola! Soy el div")
+ })
